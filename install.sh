5047c71a31f751fefb8db86e0a4eba8cb2aff614
Kazuhiro Oinuma <oinume@gmail.com>
2014-05-09 21:10:52 +0900

    Fix symlink problem

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -d ~/.zprezto ]; then
+if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
     ln -s ~/dotfiles/prezto ~/.zprezto
 fi
 
-for file in .z* .gitconfig; do
+for file in .z* .gitconfig .tmux.conf; do
     rm -i ~/$file
     ln -s $PWD/$file ~/$file
 done

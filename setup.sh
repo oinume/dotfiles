e407f51889af53e40fd2eba2fbb943a3ae1e8313
Kazuhiro Oinuma <oinume@gmail.com>
2025-02-21 18:13:45 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
-    ln -s ~/dotfiles/prezto ~/.zprezto
-fi
+# if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
+#     ln -s ~/dotfiles/prezto ~/.zprezto
+# fi
 
-if [ ! -L ~/zaw -a -d ~/dotfiles/zaw ]; then
-    ln -s ~/dotfiles/zaw ~/zaw
-fi
+# if [ ! -L ~/zaw -a -d ~/dotfiles/zaw ]; then
+#     ln -s ~/dotfiles/zaw ~/zaw
+# fi
 
-if [ ! -f ~/.percol.d/rc.py ]; then
-    mkdir -p ~/.percol.d/
-    ln -s $PWD/.percol.d/rc.py ~/.percol.d/rc.py
-fi
+# if [ ! -f ~/.percol.d/rc.py ]; then
+#     mkdir -p ~/.percol.d/
+#     ln -s $PWD/.percol.d/rc.py ~/.percol.d/rc.py
+# fi
 
 if [ ! -L ~/.tmuxinator -a -d ~/dotfiles/.tmuxinator ]; then
     ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
 fi
 
-for file in .bash* .fzf.* .z* .gitconfig .tmux.conf; do
+for file in .bash* .fzf.* .z* .gitconfig .tmux.conf Brewfile; do
     rm -i ~/$file
     ln -s $PWD/$file ~/$file
 done

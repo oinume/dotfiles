806b20fcccfb8116b0af20c7843df0ae9fec1f31
Kazuhiro Oinuma <oinume@gmail.com>
2025-08-30 20:56:36 +0900

    Add macos setting command

--- macos.sh
+++ macos.sh
@@ -4,7 +4,7 @@ defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowTabView -bool true
 defaults write com.apple.finder AppleShowAllFiles YES
-shell: chflags nohidden ~/Library
+chflags nohidden ~/Library
 
 # Change KeyRepeat speed
 defaults write -g InitialKeyRepeat -int 15
@@ -12,3 +12,18 @@ defaults write -g KeyRepeat -int 2
 
 # Change screencapture save location
 mkdir -p ~/Documents/screenshot && defaults write com.apple.screencapture location ~/Documents/screenshot
+
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick 0
+# タップでクリック
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 1
+# 副ボタンのクリック「右下隅をクリック」
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick 2
+
+# Dockの画面上の位置を「左」にする
+defaults write com.apple.dock orientation -string "left"
+# Dockのサイズ
+defaults write com.apple.dock tilesize -int 37
+# Dock拡大オン
+defaults write com.apple.dock magnification -bool true
+# Dock拡大サイズ
+defaults write com.apple.dock largesize -int 58

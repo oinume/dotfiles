2b11b1d7708e5eb0207805ea5e19afa7785b73e4
Kazuhiro Oinuma <oinume@gmail.com>
2026-01-20 09:57:37 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ if [ ! -L ~/.tmuxinator -a -d ~/dotfiles/.tmuxinator ]; then
     ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
 fi
 
+mkdir -p ~/.config/git
 for file in .bash* .fzf.* .z* .gitconfig .psqlrc .tmux.conf Brewfile .config/git/ignore; do
     rm -i ~/$file
     ln -s $PWD/$file ~/$file

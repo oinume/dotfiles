b6bdab4adac411c696f000550f8d1437bb6bd079
Kazuhiro Oinuma <oinume@gmail.com>
2026-02-23 19:46:41 +0900

    Update git-wt.sh

--- git-wt.sh
+++ git-wt.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
 # Set up git-wt in git repo
+#
 
 git config wt.basedir "../.wt-{gitroot}"
+
+
+git config --add wt.copy ".env.*"
+git config --add wt.copy ".env"
+git config --add wt.copy ".envrc"
